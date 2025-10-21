544722096ae303079437ed74694f0e825d5f02c2
aag23s019 <avakyanag@student.bmstu.ru>
2025-10-21 17:58:18 +0300

    Добавлено доп задание без разбиения на функции

--- hw2.go
+++ hw2.go
@@ -5,20 +5,23 @@ import "fmt"
 const maxNum = 12306
 
 func main() {
-
-	fmt.Printf("Введите число, не превышающее %d:  ", maxNum)
 	var number int
 	fmt.Scan(&number)
+	fmt.Println(makeWords(number))
+	// fmt.Printf("Введите число, не превышающее %d:  ", maxNum)
+	// var number int
+	// fmt.Scan(&number)
 
-	if number > maxNum {
-		fmt.Println("Ошибка. Введённое число превышает границу")
+	// if number > maxNum {
+	// 	fmt.Println("Ошибка. Введённое число превышает границу")
 
-	} else {
-		var result int = execute(number)
-		if result != 0 {
-			fmt.Printf("Ответ: %d", result)
-		}
-	}
+	// } else {
+	// 	var result int = execute(number)
+	// 	if result != 0 {
+	// 		fmt.Printf("Ответ: %d\n", result)
+	// 		fmt.Println(makeWords(result))
+	// 	}
+	// }
 
 }
 
@@ -51,4 +54,67 @@ func execute(num int) int {
 		iteration++
 	}
 	return num
+}
+
+func makeWords(num int) string {
+	result := ""
+
+	var from1To19 = []string{"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
+		"одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"}
+	var less100 = []string{"десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"}
+	var thousands = []string{"", "тысяча", "тысячи", "тысяч"}
+
+	if num == 0 {
+		result = "нолик"
+	}
+
+	if num >= 20000 && num < 100000 {
+		ending := (num / 1000) % 10
+		if ending == 0 {
+			result += less100[(num/10000)-1] + " "
+		} else if ending == 1 {
+			result += less100[(num/10000)-1] + " "
+		} else if ending == 2 {
+			result += less100[(num/10000)-1] + " "
+		} else {
+			result += less100[(num/10000)-1] + from1To19[(num/100000)%10] + " "
+		}
+		num %= 10000
+	}
+
+	if num >= 1000 && num < 20000 {
+		ending := (num / 1000) % 10
+		if ending == 1 {
+			result += "одна" + " " + thousands[1] + " "
+		} else if ending >= 2 && ending <= 4 {
+			result += from1To19[(num/1000)] + " " + thousands[2] + " "
+		} else {
+			result += from1To19[(num/1000)] + " " + thousands[3] + " "
+		}
+		num %= 1000
+	}
+
+	if num >= 100 {
+		ending := (num / 100) % 10
+		switch ending {
+		case 1:
+			result += "сто" + " "
+		case 2:
+			result += "двести" + " "
+		case 3:
+			result += "триста" + " "
+		case 4:
+			result += "четыреста" + " "
+		case 5, 6, 7, 8, 9:
+			result += from1To19[num/100] + "сот" + " "
+		}
+		num %= 100
+	}
+
+	if num >= 20 {
+		result += less100[(num/10)-1] + " "
+		num %= 10
+	}
+	result += from1To19[num]
+	return result
 }
